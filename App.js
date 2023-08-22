92d4f93886e8d3392bc4769f4f279d38af6cec41
estagiario_ti <gabrielvieirastutz@gmail.com>
2023-08-22 14:15:38 -0300

    Third lesson react native course

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
+import React from 'react';
 import { StatusBar, SafeAreaView, View} from 'react-native';
-import Cesta from './src/telas/cesta';
 import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+import AppLoading from 'expo-app-loading';
+
+import Cesta from './src/telas/Cesta';
+import mock from './src/mocks/cesta';
 
 export default function App() {
   const [fontCarregada] = useFonts({
@@ -8,12 +12,12 @@ export default function App() {
     "MontserratBold": Montserrat_700Bold,
   });
 if(!fontCarregada) {
-  return <View/>
+  return <AppLoading />
 }
   return (
     <SafeAreaView>
       <StatusBar />
-      <Cesta />
+      <Cesta {...mock}/>
     </SafeAreaView>
   );
 }
---
+++ src/telas/Cesta/componentes/Detalhes.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { View, Image, StyleSheet } from "react-native";
+
+import Texto from "../../../componentes/texto";
+
+export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, preco}) {
+    return <>
+        <View style = {estilos.cesta}>
+            <Texto style = {estilos.nome}>{nome}</Texto>
+            <View style = {estilos.fazenda}>
+                <Image source={logoFazenda} style = {estilos.imagemFazenda}/>
+                <Texto style = {estilos.nomeFazenda}>{nomeFazenda}</Texto>
+            </View>    
+            <Texto style = {estilos.descricao}>{descricao}</Texto>
+            <Texto style = {estilos.preco}>{preco}</Texto>
+        </View>
+    </>
+}
+
+const estilos = StyleSheet.create({
+    nome: {
+        fontSize: 26,
+        color: "#464646",
+        lineHeight: 42,
+        fontWeight: "bold",
+    },
+    fazenda: {
+        flexDirection: "row",
+        paddingVertical: 12,
+    },
+    imagemFazenda: {
+        width: 32,
+        height: 32,
+    },
+    nomeFazenda: {
+        fontSize: 16,
+        lineHeight: 26,
+        color: "#A3A3A3",
+        marginLeft: 12,
+        fontWeight: "bold",
+    },
+    descricao: {
+        fontSize: 16,
+        lineHeight: 26,
+        color: "#A3A3A3",
+    },
+    preco: {
+        fontSize: 26,
+        lineHeight: 42,
+        fontWeight: "bold",
+        color: "#2A9F85",
+        marginTop: 8,
+    }
+})
